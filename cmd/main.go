cb13940ca645033d6883d50d084b749741215079
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-04 15:44:42 +0500

    add .env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"log"
+	"context"
 	"os"
 	"rest"
 	"rest/pkg/handler"
 
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	srv := new(rest.Server)
 	handlers := new(handler.Handler)
+	log := logrus.New()
 
 	if err := godotenv.Load(".env"); err != nil {
 		panic(err)
 	}
-
 	port := os.Getenv("PORT")
 
-	log.Println("Server start on port" + port)
+	log.Infoln("Server start on port" + port)
 	log.Fatal(srv.Run(port, handlers.InitRouter()))
+	log.Fatal(srv.Shutdown(context.Background()))
 }
