5a48c64f3a9f795a3df73fcf19bf57083fdd464e
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-06 00:02:59 +0500

    create singup handler

--- models/user.go
+++ models/user.go
@@ -3,5 +3,5 @@ package models
 type Users struct {
 	ID       uint   `json:"id"`
 	Username string `json:"username"`
-	Password string `json:"password"`
+	Password []byte `json:"-"`
 }
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"rest/database"
+	"rest/models"
+
 	"github.com/labstack/echo/v4"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
 )
-
 
 func (h *Handler) SingUp(c echo.Context) error {
 	var data map[string]string
@@ -14,7 +16,14 @@ func (h *Handler) SingUp(c echo.Context) error {
 
 	password, _ := bcrypt.GenerateFromPassword([]byte(data["passwordl"]), 14)
 
-	return nil
+	user := models.Users{
+		Username: data["username"],
+		Password: password,
+	}
+
+	database.DB.Create(&user)
+
+	return c.JSON(200, user)
 }
 
 func (h *Handler) SingIn(c echo.Context) error {
