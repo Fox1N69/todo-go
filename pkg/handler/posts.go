9b8e358eba992abdf794057f0afd56d91ac87e09
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-09 14:11:16 +0500

    c.Param(id) to c.Param(ID)

--- pkg/handler/posts.go
+++ pkg/handler/posts.go
@@ -38,7 +38,7 @@ func (h *Handler) setPost(c echo.Context) error {
 
 func (h *Handler) updatePosts(c echo.Context) error {
 	var data []models.Posts
-	id := c.Param("id")
+	id := c.Param("ID")
 
 	if err := database.DB.First(&data, id); err != nil {
 		log.Fatal("Error id receving ", err)
