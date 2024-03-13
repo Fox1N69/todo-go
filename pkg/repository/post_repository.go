31e22739f96af4f7d92d764ae72c4e7604619445
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-13 22:18:34 +0500

    change postrepository functions

--- pkg/repository/post_repository.go
+++ pkg/repository/post_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"rest/database"
 	"rest/pkg/models"
 
@@ -8,21 +9,34 @@ import (
 )
 
 type PostRepository struct {
+	db *gorm.DB
 }
 
 type PostRepositoryI interface {
 	CreatePost(post models.Posts) error
-	GetPostByID(id uint) (*gorm.DB, error)
+	GetPostByID(id uint) (models.Posts, error)
 	UpdatePost(post models.Posts) error
 	DeletePost(post models.Posts) error
 }
 
+func NewPostRepository(db *gorm.DB) *PostRepository {
+	return &PostRepository{db: db}
+}
+
 func (pr *PostRepository) Create(post *models.Posts) error {
+	if pr == nil {
+		return errors.New("PostREpository is null")
+	}
 	return database.DB.Create(post).Error
 }
 
-func (pr *PostRepository) GetByID(id uint) (*gorm.DB, error) {
-	return nil, nil
+func (pr *PostRepository) GetByID(id uint) (*models.Posts, error) {
+	var post models.Posts
+	if err := database.DB.Where("id = ?", id).First(&post).Error; err != nil {
+		return nil, err
+	}
+
+	return &post, nil
 }
 
 func (pr *PostRepository) UpdatePost(postToUpdate models.Posts) error {
@@ -30,5 +44,5 @@ func (pr *PostRepository) UpdatePost(postToUpdate models.Posts) error {
 }
 
 func (pr *PostRepository) DeletePost(post models.Posts) error {
-	return nil
+	return database.DB.Delete(post).Error
 }
