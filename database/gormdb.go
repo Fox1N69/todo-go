0e41c6e0790123b9405367a6873707702f2c6157
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-17 18:00:20 +0500

    add air

--- database/gormdb.go
+++ database/gormdb.go
@@ -14,9 +14,9 @@ var dsn = "user=postgres password=8008 dbname=sitesb port=5432 sslmode=disable"
 
 func InitGormDB() *gorm.DB {
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed connect to database", err)
 	}
 
 	DB.AutoMigrate(
