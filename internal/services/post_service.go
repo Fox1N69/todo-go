d58441e990aae80131300bbe48c89943bf172004
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-28 19:30:58 +0500

    create the service for posts

--- internal/services/post_service.go
+++ internal/services/post_service.go
@@ -1,6 +1,9 @@
 package services
 
-import "blog/internal/repositorys"
+import (
+	"blog/internal/repositorys"
+	"blog/pkg/models"
+)
 
 type PostServeci struct {
 	postRepository *repositorys.PostRepository
@@ -8,4 +11,20 @@ type PostServeci struct {
 
 func NewPostServeci(postRepo *repositorys.PostRepository) *PostServeci {
 	return &PostServeci{postRepository: postRepo}
+}
+
+func (s *PostServeci) GetAllPosts() ([]models.Post, error) {
+	return nil, nil
+}
+
+func (s *PostServeci) CreatePost(post models.Post) error {
+	return nil
+}
+
+func (s *PostServeci) UpdatePost(id uint, post models.Post) error {
+	return nil
+}
+
+func (s *PostServeci) DeletePost(id uint) error {
+	return nil
 }
