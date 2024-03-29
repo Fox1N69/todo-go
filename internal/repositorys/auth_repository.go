1acabb8d2abe5b486c3d85393b8a28d0adb6c551
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-29 11:41:17 +0500

    add repository user

--- internal/repositorys/auth_repository.go
+++ internal/repositorys/auth_repository.go
@@ -1,6 +1,10 @@
 package repositorys
 
-import "gorm.io/gorm"
+import (
+	"blog/pkg/models"
+
+	"gorm.io/gorm"
+)
 
 type AuthRepository struct {
 	DB *gorm.DB
@@ -8,4 +12,20 @@ type AuthRepository struct {
 
 func NewAuthRepository(db *gorm.DB) *AuthRepository {
 	return &AuthRepository{DB: db}
+}
+
+func (r *AuthRepository) Create(user *models.User) error {
+	return nil
+}
+
+func (r *AuthRepository) GetByID(id uint) ([]models.User, error) {
+	return nil, nil
+}
+
+func (r *AuthRepository) Update(user *models.User) error {
+	return nil
+}
+
+func (r *AuthRepository) Delete(user *models.User) error {
+	return nil
 }
