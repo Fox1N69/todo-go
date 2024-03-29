4fb772e186ba15930880224d9f6996b02e38e148
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-29 11:51:34 +0500

    Auth service has been create

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -1,6 +1,9 @@
 package services
 
-import "blog/internal/repositorys"
+import (
+	"blog/internal/repositorys"
+	"blog/pkg/models"
+)
 
 type AuthService struct {
 	repository *repositorys.AuthRepository
@@ -10,3 +13,18 @@ func NewAuthService(authRepo *repositorys.AuthRepository) *AuthService {
 	return &AuthService{repository: authRepo}
 }
 
+func (s *AuthService) Login(user models.User) error {
+	return nil
+}
+
+func (s *AuthService) Singup() error {
+	return nil
+}
+
+func (s *AuthService) Logout() error {
+	return nil
+}
+
+func (s *AuthService) Refresh() error {
+	return nil
+}
