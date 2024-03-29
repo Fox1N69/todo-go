62593dd4203a58a2da17cd39d4f914942ca72207
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-29 12:28:38 +0500

    hashed password and ckeckhashed password

--- internal/controller/auth_controller.go
+++ internal/controller/auth_controller.go
@@ -1,11 +1,19 @@
 package controller
 
+import "golang.org/x/crypto/bcrypt"
+
 type AuthController struct{}
 
 var secretKey = []byte("secret")
 
-func hashPassword(password string) (string, error) {
-	return "", nil
+func hashPassword(password []byte) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	return hash, err
+}
+
+func checkPasswordHash(password, hash []byte) bool {
+	err := bcrypt.CompareHashAndPassword(hash, password)
+	return err == nil
 }
 
 func (c *AuthController) Login() error {
