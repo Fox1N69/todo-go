97de052bb340adac46642130b3f5e7f0e8d0f704
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-29 12:17:47 +0500

    controller

--- internal/controller/auth_controller.go
+++ internal/controller/auth_controller.go
@@ -1,15 +1,13 @@
 package controller
 
-type AuthController struct {
-}
+type AuthController struct{}
 
-func hashPassword() error {
-	return nil
+func hashPassword(password string) (string, error) {
+	return "", nil
 }
 
 func (c *AuthController) Login() {
 }
 
 func (c *AuthController) Register() {
-
 }
