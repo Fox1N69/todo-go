bfd4fe03acc9ec92b28e42fd45a758f265da7bec
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2024-03-29 12:20:54 +0500

    add secretKey

--- internal/controller/auth_controller.go
+++ internal/controller/auth_controller.go
@@ -2,12 +2,16 @@ package controller
 
 type AuthController struct{}
 
+var secretKey = []byte("secret")
+
 func hashPassword(password string) (string, error) {
 	return "", nil
 }
 
-func (c *AuthController) Login() {
+func (c *AuthController) Login() error {
+	return nil
 }
 
-func (c *AuthController) Register() {
+func (c *AuthController) Register() error {
+	return nil
 }
